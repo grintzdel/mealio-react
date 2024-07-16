391acfb08111c0a394456cb18916b3a16df55797
grintzdel <mathis.oudin@etu.uca.fr>
2024-07-16 21:03:33 +0200

    :lipstick: add navbar & footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
+import styles from './Footer.module.scss';
+
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <footer className={`${styles.footer} flex row align-center justify-center p-20`}>
+            <p>© 2024, Mealio</p>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,22 @@
+import styles from './Header.module.scss';
+import logo from '../assets/images/mealio.svg';
+
 function Header() {
-    return <div>Header</div>;
+    return (
+        <header className={`${styles.header} flex row align-center`}>
+            <i className="fa-solid fa-bars mr-15"></i>
+            <div className="flex-fill">
+                <img src={logo} alt="Logo Mealio"/>
+            </div>
+            <ul>
+                <button className="mr-5 btn btn-primary">
+                    <i className="fa-solid fa-basket-shopping mr-5"></i>
+                    <span>Panier</span>
+                </button>
+                <button className="btn btn-secondary">Connexion</button>
+            </ul>
+        </header>
+    );
 }
 
 export default Header;
